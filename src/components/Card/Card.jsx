a55ef8d12d41f4874b64806130fb4a6016b0fa69
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-12-06 15:31:27 +0100

    ADD content

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+
+import placeholderImg from "../../assets/placeholder.webp"
+import style from "./card.module.css"
+import { API_BASE_URI } from '../../pages/Posts/Posts'
+
+import { useState } from 'react'
+
+
+export default function Card({ deletePost=()=>{}, changePostTitle=()=>{}, setPostTitle=()=>{}, title ="", image, content="",tags="", postTitle="", id}){
+
+
+    const [isForm, setIsForm] = useState(false)
+
+    function showForm(){
+        setIsForm(!isForm)
+    }
+
+    return(
+        <div className={style.card}>
+            <div className={style.col12}>
+                <img className={style.thumb} src={image ? `${API_BASE_URI}/imgs/posts/` + image : placeholderImg}/>
+            </div>
+            <div className={style.col12}>
+                <div className={style.cardBody}>
+                    <div className={style.titleSection}>
+                        {!isForm ? <h3>{title}</h3> : <form onSubmit={(e) => {e.preventDefault(); changePostTitle(id); showForm();}} action="">
+                            <input onChange={(e)=>setPostTitle(e.target.value)} type="text" placeholder="Nuovo titolo del post" value={postTitle}/>
+                            <input className={style.submit} type="submit" value="Invia"/>
+                        </form>}
+                        <button onClick={showForm}><FontAwesomeIcon icon={faPen} className={style.faPen}/></button>
+                    </div>  
+                    <p>{content}</p>
+                    {tags.map((tag,index) => <p className={`tag ${tag}Tag`} key={index}>{tag}</p>)}
+                    <div className={style.btnSection}>
+                        <button><a href="#">Leggi di più</a></button>
+                        <button onClick={()=>deletePost()}><FontAwesomeIcon icon={faTrash} className={style.faTrash}/></button>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,41 +1,167 @@
-import commonStyle from '../Common.module.css'
 import style from './Posts.module.css'
+import Card from "../../components/Card/Card"
+
+import axios from "axios"
+
+import { useState, useEffect/*, useRef*/ } from 'react'
+
+const baseFormData={
+    title:"",
+    image:"",
+    content:"",
+    tags:[],
+    published:true
+}
+
+export const API_BASE_URI = 'http://localhost:3000/'
 
 export default function Posts(){
-    const posts=[
-        {
-            title:"Post 1",
-            content:"Content 1"
-        },
-        {
-            title:"Post 2",
-            content:"Content 2"
-        },
-        {
-            title:"Post 3",
-            content:"Content 3"
-        },
-        {
-            title:"Post 4",
-            content:"Content 4"
+
+    let tags=[]
+    const [posts, setPosts] = useState([])
+    const [postTitle, setPostTitle] = useState("")
+
+    const [formData, setFormData] = useState(baseFormData)
+
+    //const isLoaded = useRef(false);
+
+    function fetchPosts(){
+        axios.get(`${API_BASE_URI}posts`)
+            .then(res => {
+                console.log("fetch posts")
+                console.log(res.data)
+                setPosts(res.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    },[])
+
+    /*
+    useEffect(() => {
+        if(isLoaded.current){
+            alert("Toggle Visibilità")
         }
-    ]
+        else{
+            isLoaded.current=true;}
+            return
+    },[formData.published])
+    */
+
+    function handleFormData(e){
+
+        const key = e.target.name
+        const value = e.target.value
+        const type = e.target.type
+        const checked = e.target.checked
+
+        setFormData((formData) =>{
+            const newFormData = {...formData}
+
+            type === "checkbox" && key =="tags" ? 
+            (newFormData.tags = checked ? formData.tags.concat(value) : formData.tags.filter((tag) => tag!==value)) 
+            : type === "checkbox" && key ==="published" ? 
+            (newFormData.published = checked) 
+            : (newFormData[key] = value)
+
+            return newFormData;
+        })
+    }
+
+    function addNewPost(){
+
+        if(formData.title === "") return
+
+        const newPost ={
+            ...formData
+        }
+
+        axios.post(`${API_BASE_URI}posts`, newPost)
+            .then(res =>{
+                console.log(res)
+                console.log("post aggiunto correttamente")
+                setPosts([...posts, res.data])
+                setFormData(baseFormData)
+            })
+            .catch(err =>{
+                console.error(err)
+            })
+
+        //console.log(posts)
+        fetchPosts()
+    }
+
+    function deletePost(id){
+        console.log(id)
+        axios.delete(`${API_BASE_URI}posts/${id}`)
+            .then(()=>{
+                fetchPosts()
+            })
+            .catch(err =>{
+                console.error(err)
+            })
+    }
+
+    function changePostTitle(id){
+        if(postTitle === "") return
+
+        setPosts(posts.map(post => 
+            post.id === id ? { ...post, title: postTitle } : post
+        ));
+        setPostTitle("")
+
+        //console.log(posts)
+        console.log("Titolo aggiornato correttamente");
+    }
+
     return(
-        <main>
-            <div className="container">
-                <section>
-                    <h1 className={commonStyle.title}>POSTS</h1>
-                </section>
-                <section className={style.postsList}>
-                    {posts.map((post,i) =>(
-                        <div key={i} className={style.post}>
-                            <h2>{post.title}</h2>
-                            <p>{post.content}</p>
-                        </div>
-                    ))}
-                </section>
-            </div>
-            
-        </main>
+        <main className={style.mainContent}>
+        <div className="container">
+            <form className={style.form} onSubmit={(e) => {e.preventDefault(); addNewPost()}} action="">
+                <div className={style.formDiv}>
+                    <input className={style.formField} id="title" name="title" onChange={handleFormData} type="text" placeholder="Titolo del post" value={formData.title}/>
+                </div>
+                <div className={style.formDiv}>
+                    <input className={style.formField} id="content" name="content" onChange={handleFormData} type="text" placeholder="Descrizione del post" value={formData.content}/>
+                </div>
+                <div className={style.formDiv}>
+                    <input className={style.formField} id="image" name="image" onChange={handleFormData} type="text" placeholder="URL immagine" value={formData.image}/>
+                </div>
+                <div className={style.formDiv}>
+                    <input  id="tag1" name="tags" onChange={handleFormData} type="checkbox" value={"tag1"}/>
+                    <label className={style.formCheckbox} htmlFor="tag1">Tag 1</label>
+                    <input id="tag2" name="tags" onChange={handleFormData} type="checkbox" value={"tag2"}/>
+                    <label className={style.formCheckbox} htmlFor="tag2">Tag 2</label>
+                    <input id="tag3" name="tags" onChange={handleFormData} type="checkbox" value={"tag3"}/>
+                    <label className={style.formCheckbox} htmlFor="tag3">Tag 3</label>
+                </div>
+                <div className={style.formDiv}>
+                    <input id="published" name="published" onChange={handleFormData} checked={formData.published} type="checkbox"/>
+                    <label htmlFor="published">Visibile</label>
+                </div>
+                <input className={style.submit} type="submit" value="Invia"/>
+            </form>
+
+            <section className={style.row}>
+                {posts.map((post)=>
+                    post.published===true ?
+                    <div className={style.col3} key={post.id}>
+                        <Card deletePost={()=>deletePost(post.id)} changePostTitle={(event)=>changePostTitle(event, post.id)} setPostTitle={(value)=>setPostTitle(value)} title={post.title} image={post.image} content={post.content} tags={post.tags} postTitle={postTitle} id={post.id}/>
+                        {post.tags.forEach(tag => !tags.find((el) => el === tag) ? tags.push(tag) : null)}
+                    </div> : null
+                )}
+            </section>
+            <section className={style.tagList}>
+                <h3>Tag presenti</h3>
+                <ul>
+                    {tags.map((tag, index) => <li className={`tag ${tag}Tag`} key={index}>{tag}</li> )}
+                </ul>
+            </section>
+        </div>
+    </main>
     )
 }
