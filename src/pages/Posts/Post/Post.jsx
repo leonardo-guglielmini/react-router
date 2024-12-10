6f218210b83b4b09711ec0e7e536c4b8235bc8e1
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-12-10 16:26:18 +0100

    ADD post.jsx content

--- src/pages/Posts/Post/Post.jsx
+++ src/pages/Posts/Post/Post.jsx
@@ -1,7 +1,9 @@
 import placeholderImg from "../../../assets/placeholder.webp"
+import loadingImg from "../../../assets/loading.gif"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import style from "./post.module.css"
 
 const API_BASE_URI = 'http://localhost:3000/'
 
@@ -15,6 +17,7 @@ export default function Post(){
         axios.get(`${API_BASE_URI}posts/${id}`)
         .then((res) =>{
             setPost(res.data)
+            //console.log(res.data)
         })
         .catch((err) =>{
             console.error(err)
@@ -22,8 +25,24 @@ export default function Post(){
     },[id])
 
     return(
-        <main>
+        <main className={style.mainContent}>
             <div className="container">
+            {
+                post ? 
+                <div className={style.card}>
+                    <div className={style.cardImg}>
+                        <img src={post.image ? `${API_BASE_URI}/imgs/posts/` + post.image : placeholderImg}></img>
+                    </div>
+                    <div className={style.cardBody}>
+                        <h1>{post.title}</h1>
+                        <p>{post.content}</p>
+                    </div>
+                </div> :
+                <div className={style.loading}>
+                    <img src={loadingImg}></img>
+                </div>
+            }
+                
             </div>
         </main>
     )}
